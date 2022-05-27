20ac309163a8337a72ef014c0cff8312b1056c76
Jeremy Samuel <bubbowrap@gmail.com>
2022-05-26 21:25:42 -0400

    feat: app login page/more setup.

---
+++ src/pages/AppLogin.tsx
@@ -0,0 +1,18 @@
+import GlobalStyles from '@mui/material/GlobalStyles';
+
+const AppLogin = () => {
+  return (
+    <>
+      <GlobalStyles
+        styles={{
+          body: {
+            background: 'linear-gradient(90deg, #2839BD 0.08%, #4D29C1 100%);',
+          },
+        }}
+      />
+      ;
+    </>
+  );
+};
+
+export default AppLogin;
---
+++ src/styles/theme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#3539BD',
+    },
+    background: {
+      default: '#191D25',
+    },
+  },
+});
+
+export default theme;
